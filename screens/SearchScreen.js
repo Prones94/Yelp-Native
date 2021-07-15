649ce3309ab5b25da0e4776145e2e0239e7acbc4
Ian Rones <ronesp.85@gmail.com>
2021-07-15 10:32:26 -0700

    [add] included testing for any errors in searching

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -6,9 +6,11 @@ import yelp from '../api/yelp'
 export default function SearchScreen() {
   const [term, setTerm] = useState('')
   const [results, setResults] = useState([])
+  const [errorMessage, setErrorMessage] = useState('')
 
   const searchAPI = async () => {
-    const response = await yelp.get('/search', {
+    try {
+      const response = await yelp.get('/search', {
       params: {
         limit: 50,
         term,
@@ -16,6 +18,9 @@ export default function SearchScreen() {
       }
     });
     setResults(response.data.businesses)
+  } catch (err) {
+    setErrorMessage('Something went wrong')
+  }
   }
 
   return (
@@ -25,7 +30,7 @@ export default function SearchScreen() {
         onTermChange={setTerm}
         onTermSubmit={searchAPI}
       />
-      <Text>Search Screen Updated</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
     </View>
   )
