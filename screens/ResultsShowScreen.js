3ef3fcafcab0bb9e997a8067bacb0198eebd8e4b
Ian Rones <ronesp.85@gmail.com>
2021-07-16 12:23:56 -0700

    [add] phone number and address to single restaurant search

--- screens/ResultsShowScreen.js
+++ screens/ResultsShowScreen.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react'
-import { View, Text, StyleSheet, FlatList, Image } from'react-native'
+import { View, Text, StyleSheet, FlatList, Image } from 'react-native'
 import yelp from '../api/yelp'
 
-const ResultsShowScreen = ({ route,navigation }) => {
+const ResultsShowScreen = ({ route, navigation }) => {
   const [result, setResult] = useState(null)
   const id = route.params?.id ?? 'null'
   console.log(id)
@@ -21,22 +21,45 @@ const ResultsShowScreen = ({ route,navigation }) => {
 
   return (
     <View>
-      <Text>{result.name}</Text>
+      <Text style={styles.headingStyle}>{result.name}</Text>
+      <View style={{ borderBottomColor: 'black', marginBottom: 15, borderBottomWidth: 1 }} />
       <FlatList
         data={result.photos}
+        horizontal
         keyExtractor={photo => photo}
-        renderItem = {({item}) => {
-          return <Image style={styles.imageStyle} source={{ uri: item}} />
+        renderItem={({ item }) => {
+          return <Image style={styles.imageStyle} source={{ uri: item }} />
         }}
       />
+      <View style={styles.resultStyle}>
+        <Text style={styles.textStyle}>Phone: {result.phone}</Text>
+        <Text style={styles.textStyle}>Address: {result.location.address1}</Text>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   imageStyle: {
+    height: 150,
+    width: 300,
+    padding: 20,
+    marginHorizontal: 10
+  },
+  headingStyle: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    alignSelf: 'center',
+    marginBottom: 30,
+    marginTop: 20
+  },
+  resultStyle: {
     height: 200,
-    width:300
+    flexDirection: 'column',
+    padding: 20
+  },
+  textStyle: {
+    fontWeight: 'bold',
   }
 })
 
